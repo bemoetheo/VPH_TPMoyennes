8ffc6497300261960cf2fc61fe6044dacb006a16
VHP <p.vuhoang000@gmail.com>
2023-10-24 15:13:26 +0200

    Final build

--- Classe.cs
+++ Classe.cs
@@ -4,9 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HNI_TPmoyennes
+namespace TPMoyennes
 {
-    internal class Classe1
+    class Classe
     {
+        public string nomClasse;
+        public List<Eleve> eleves = new List<Eleve>();
+        public List<string> matieres = new List<string>();
+
+        public Classe(string classeNom)
+        {
+            nomClasse = classeNom;
+        }
+        public void ajouterEleve(string fname, string lname)
+        {
+            if (eleves.Count >= 30)
+            {
+                Console.WriteLine("Erreur : Une classe n'accueille qu'au maximum 30 eleves");
+                throw new Exception("Une classe n'accueille qu'au maximum 30 eleves");
+            }
+            eleves.Add(new Eleve { prenom = fname, nom = lname });
+        }
+        public void ajouterMatiere(string matiere)
+        {
+            if (matieres.Count >= 10)
+            {
+                Console.WriteLine("Erreur : 10 matieres maximum sont enseignees dans une classe");
+                throw new Exception("10 matieres maximum sont enseignees dans une classe");
+            }
+            matieres.Add(matiere);
+        }
+        public decimal moyenneMatiere(int m)
+        {
+            float noteTotMatClasse = 0;
+            for (int ieleve = 0; ieleve < eleves.Count; ieleve++)
+            {
+                float noteTotMatEleve = 0;
+                int counter = 0;
+                for (int inote = 0; inote < eleves[ieleve].toutesNotes.Count; inote++)
+                {
+                    if (eleves[ieleve].toutesNotes[inote].matiere == m)
+                    {
+                        noteTotMatEleve += eleves[ieleve].toutesNotes[inote].note;
+                        counter++;
+                    }
+                }
+                noteTotMatClasse += noteTotMatEleve / counter;
+            }
+            return Math.Round((decimal)(noteTotMatClasse / eleves.Count), 2);
+        }
+        public decimal moyenneGeneral()
+        {
+            float noteGenClasse = 0;
+            for (int ieleve = 0; ieleve < eleves.Count; ieleve++)
+            {
+                float noteTotalEleve = 0;
+                for (int inote = 0; inote < eleves[ieleve].toutesNotes.Count; inote++)
+                {
+                    noteTotalEleve += eleves[ieleve].toutesNotes[inote].note;
+                }
+                noteGenClasse += noteTotalEleve / eleves[ieleve].toutesNotes.Count;
+            }
+            return Math.Round((decimal)(noteGenClasse / eleves.Count), 2);
+        }
     }
 }
---
+++ Eleve.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMoyennes
+{
+    class Eleve
+    {
+        private string _prenom;
+        private string _nom;
+        public List<Note> toutesNotes = new List<Note>();
+        public string prenom
+        {
+            get => _prenom;
+            set => _prenom = value;
+        }
+        public string nom
+        {
+            get => _nom;
+            set => _nom = value;
+        }
+
+        public decimal moyenneGeneral()
+        {
+            float noteTotal = 0;
+            for (int imatiere = 0; imatiere < toutesNotes.Count; imatiere++)
+            {
+                noteTotal += toutesNotes[imatiere].note;
+            }
+            return Math.Round((decimal)(noteTotal / toutesNotes.Count), 2);
+        }
+        public decimal moyenneMatiere(int m)
+        {
+            float noteTotMat = 0;
+            int counter = 0;
+            for (int inote = 0; inote < toutesNotes.Count; inote++)
+            {
+                if (toutesNotes[inote].matiere == m)
+                {
+                    noteTotMat += toutesNotes[inote].note;
+                    counter++;
+                }
+            }
+            return Math.Round((decimal)(noteTotMat / counter), 2);
+        }
+        public void ajouterNote(Note noteEleve)
+        {
+            if (toutesNotes.Count >= 200)
+            {
+                Console.WriteLine("Erreur : Un eleve recoit au plus 200 notes au cours de l'annee");
+                throw new Exception("Un eleve recoit au plus 200 notes au cours de l'annee");
+            }
+            toutesNotes.Add(noteEleve);
+        }
+    }
+}
